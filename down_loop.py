a327349601772557fc72784da15c803cef39ed7d
Allex Radu <eusceptic@gmail.com>
2020-07-21 16:31:24 +0300

    updating code for multiple columns

--- down_loop.py
+++ down_loop.py
@@ -41,7 +41,7 @@ def download_images(img_file_names, image_multiplier):
 
                         response = requests.get(excel.excel_product_image_url[i], stream = True, verify = False)
                         file_name = system_prefix + img_file_name if platform.system() == 'Windows' \
-                            else system_prefix + img_file_name.replace(' ', '\\ ')
+                            else system_prefix + img_file_name.replace(' ', r'\ ')
                         with open(file_name, 'wb') as out_file:
                             shutil.copyfileobj(response.raw, out_file)
                             print('downloading image: ', excel.excel_product_image_url[i] + ' index:' + str(i))
