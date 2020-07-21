c7679d568adbdb8d6effe802c49555f3115a7c56
Allex Radu <eusceptic@gmail.com>
2020-07-21 16:06:00 +0300

    first commit

--- down_loop.py
+++ down_loop.py
@@ -8,17 +8,16 @@ from urllib.request import Request, urlopen
 import requests
 import shutil
 
-read_urls_excel_cell_letter = 'AM'
-read_product_name_cell_letter = 'O'
-image_names_cell_letter = 'AN'
+read_urls_excel_cell_letters = ['C', 'D', 'E']
+read_product_name_cell_letter = 'B'
+image_names_cell_letters = ['F', 'G', 'H']
 
 image_file_names = []
 
-excel.read_image_urls(read_urls_excel_cell_letter)
 excel.read_product_names(read_product_name_cell_letter)
 
 
-def download_images(img_file_names):
+def download_images(img_file_names, image_multiplier):
     for i in range(1, len(excel.excel_product_image_url)):
         if excel.excel_product_image_url[i] != '':
             if excel.excel_product_image_url[i] != 'none':
@@ -32,10 +31,10 @@ def download_images(img_file_names):
                         img_suffix = img_suffix_raw[:3]
 
                         img_file_name = excel.sanitise_product_names(
-                            excel.excel_product_names[i]) + '_1' + '.' + img_suffix
+                            excel.excel_product_names[i]) + image_multiplier + '.' + img_suffix
                         if len(img_file_name) > 100:
                             img_file_name = excel.sanitise_product_names(
-                                excel.excel_product_names[i][0:100]) + '_1' + '.' + img_suffix
+                                excel.excel_product_names[i][0:100]) + image_multiplier + '.' + img_suffix
 
                         # img_file_name = img_file_name_raw
                         system_prefix = 'excel\\photos\\' if platform.system() == 'Windows' else 'excel/photos/'
@@ -55,6 +54,13 @@ def download_images(img_file_names):
                     img_file_names.append('n/a')
 
 
-download_images(image_file_names)
+def down_loop():
+    global image_file_names
+    for i in range(len(read_urls_excel_cell_letters)):
+        excel.read_image_urls(read_urls_excel_cell_letters[i])
+        download_images(image_file_names, f'_{i}')
+        excel.write_file_image_to_excel(image_file_names, image_names_cell_letters[i])
+        image_file_names = []
 
-excel.write_file_image_to_excel(image_file_names, image_names_cell_letter)
+
+down_loop()
